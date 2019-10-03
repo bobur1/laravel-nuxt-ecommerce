c2303e8f30ee9517f7d5273ecaa2f90c865eac47
Bobur Zaitov <bobur_moscow@mail.ru>
2019-10-04 00:09:23 +0300

    showing categories and children

--- cartclient/nuxt.config.js
+++ cartclient/nuxt.config.js
@@ -1,52 +1,54 @@
-
 export default {
-  mode: 'universal',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+    mode: 'universal',
+    /*
+    ** Headers of the page
+    */
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+        ]
+    },
 
 
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
+    /*
+    ** Customize the progress-bar color
+    */
+    loading: {color: '#fff'},
+    /*
+    ** Global CSS
+    */
     css: [
         '~assets/styles/app.scss'
     ],
-  plugins: [
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
-  /*
-  ** Build configuration
-  */
-  build: {
+    plugins: [],
     /*
-    ** You can extend webpack config here
+    ** Nuxt.js dev-modules
     */
-    extend (config, ctx) {
+    buildModules: [],
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
+        '@nuxtjs/axios'
+    ],
+    axios: {
+        // proxyHeaders: false
+        baseURL: 'http://laravel-mastering.test/ecommerce/public/api'
+    },
+    /*
+    ** Build configuration
+    */
+    build: {
+        /*
+        ** You can extend webpack config here
+        */
+        extend(config, ctx) {
+        }
     }
-  }
 }
---
+++ cartclient/pages/categories/_slug.vue
@@ -0,0 +1,9 @@
+<template>
+    <p>products for a category</p>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
