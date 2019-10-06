17c8e77efe5624856fb58ba41473e265c8e62f73
Bobur Zaitov <bobur_moscow@mail.ru>
2019-10-07 01:14:17 +0300

    Scoping products by category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,21 @@
 
 namespace App\Models;
 
+use App\Scoping\Scoper;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Product extends Model
 {
-    public  function getRouteKeyName()
-    {
+    public  function getRouteKeyName(){
         return 'slug';
+    }
+
+    public  function scopeWithScopes(Builder $builder, $scopes = []){
+        return (new Scoper(request()))->apply($builder, $scopes);
+    }
+
+    public function categories(){
+         return $this->belongsToMany(Category::class);
     }
 }
